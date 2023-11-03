30eb34078ca674774a16e5ced4f63fe2b0b7bb27
NjorogeAnn <149757862+NjorogeAnn@users.noreply.github.com>
2023-11-03 08:59:15 +0300

    Add files via upload

---
+++ Javascript/js/index.js
@@ -0,0 +1,7 @@
+function display(){
+    //alert("Welcome to our system!");
+    document.getElementById("heading").innerHTML="This is Javascript";
+}
+function hide(){
+    document.getElementById("heading").innerHTML="";
+}
